cdabc63d55696d78124a4b082610661600cbb9c8
Checkerb0ard <74562768+Checkerb0ard@users.noreply.github.com>
2023-08-13 02:51:59 -0700

    Initial Commit
    Upload Initial files

---
+++ UPnP/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// Information about a mod is controlled through the following set of
+// attributes. Change these attribute values to modify the information
+// associated with a mod.
+[assembly: MelonInfo(typeof(UPnP.UPnP), "UPnP", "1.0.0", "Tidefusion")]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UPnP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UPnP")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("adb4261c-9422-4fc4-9039-6e7319376a8b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UPnP/UPnP.cs
@@ -0,0 +1,135 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Open.Nat;
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UPnP
+{
+    public class UPnP : MelonMod
+    {
+        public static UPnP Singleton;
+        private NatDevice natDevice;
+        private string localIp;
+
+        private MelonPreferences_Category _UNP;
+        private MelonPreferences_Entry<string> LocalIPAddress;
+
+        public override void OnInitializeMelon()
+        {
+            _UNP = MelonPreferences.CreateCategory("UPNP");
+
+            LocalIPAddress = _UNP.CreateEntry<string>("Local IP Address", (""));
+
+            MelonLogger.Msg("Mellon Preferences loaded!");
+        }
+
+#pragma warning disable CS0672 // Member overrides obsolete member
+        public override void OnApplicationStart()
+#pragma warning restore CS0672 // Member overrides obsolete member
+        {
+            Singleton = this;
+            MelonLogger.Msg("UPnP mod has started.");
+
+            FetchAndOpenPort();
+        }
+
+        public override void OnApplicationQuit()
+        {
+            if (natDevice != null)
+            {
+                ClosePort();
+            }
+
+            MelonLogger.Msg("UPnP mod has stopped.");
+        }
+
+        private async void FetchAndOpenPort()
+        {
+            try
+            {
+                var discoverer = new NatDiscoverer();
+                var cts = new System.Threading.CancellationTokenSource(5000);
+                natDevice = await discoverer.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+
+                if (natDevice != null)
+                {
+                    string externalIp = await FetchExternalIPv4Async();
+                    localIp = FetchLocalIPv4();
+
+                    if (!string.IsNullOrEmpty(externalIp) && !string.IsNullOrEmpty(localIp))
+                    {
+                        await OpenPortAsync(natDevice, externalIp);
+                    }
+                    else
+                    {
+                        MelonLogger.Msg("Failed to fetch IP addresses.");
+                    }
+                }
+                else
+                {
+                    MelonLogger.Msg("No compatible NAT device found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Error($"Error: {ex.Message}");
+            }
+        }
+
+        private async Task<string> FetchExternalIPv4Async()
+        {
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    string response = await client.DownloadStringTaskAsync("https://api.ipify.org?format=json");
+                    return response.Trim();
+                }
+                catch (Exception ex)
+                {
+                    MelonLogger.Error($"Error fetching external IPv4 address: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        private string FetchLocalIPv4()
+        {
+            var localIp = LocalIPAddress.Value;
+
+            return localIp;
+        }
+
+        private async Task OpenPortAsync(NatDevice device, string externalIp)
+        {
+            try
+            {
+                var portmap = new Mapping(Protocol.Udp, 7777, 7777, "MelonLoader");
+                await device.CreatePortMapAsync(portmap);
+
+                MelonLogger.Msg($"Port 7777 has been opened. External IPv4: {externalIp}, Local IPv4: {localIp}");
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Error($"Error opening port: {ex.Message}");
+            }
+        }
+
+        private async void ClosePort()
+        {
+            try
+            {
+                await natDevice.DeletePortMapAsync(new Mapping(Protocol.Udp, 7777, 7777));
+                MelonLogger.Msg("Port 7777 has been closed.");
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Error($"Error closing port: {ex.Message}");
+            }
+        }
+    }
+}
